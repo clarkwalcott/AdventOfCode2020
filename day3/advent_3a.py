bc74a2fa730e3941edb04506af55cfb6a9fcbb4f
ClarkWalcott <Clark.Walcott@target.com>
2020-12-03 16:42:01 -0600

    Refactored solution.

--- day3/advent_3a.py
+++ day3/advent_3a.py
@@ -1,19 +1,8 @@
-# read input into a matrix
-terrain = open("input.txt", "r").read().splitlines()
+x, y, hit = 0, 0, 0
 
-# movement
-right = 3
-down = 1
-
-x = 0
-y = 0
-hit = 0
-# y increases by 1 until it hits the bottom
-# x increases until it hits the right side, then it resets to 0
-
-while y < len(terrain):
-    if terrain[y][x] == '#':
+for row in open("input.txt", "r").read().splitlines():
+    if row[x] == '#':
         hit +=1
-    x = (x + right) % (len(terrain[0]))
-    y += down
+    x = (x + 3) % len(row)
+    y += 1
 print(hit)
